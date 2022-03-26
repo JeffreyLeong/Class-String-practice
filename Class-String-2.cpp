bb69bca71bd2bdd13557ff5a292d1260325a62e5
dollarortwo <jleong001@gmail.com>
2022-03-26 11:17:57 -0700

    added main function

--- Class-String-2.cpp
+++ Class-String-2.cpp
@@ -6,7 +6,27 @@ void new_line();
 
 int main()
 {
+    using namespace std;
 
+    string first_name, last_name, record_name;
+    string motto = "Your records are our records.";
+
+    cout << "Enter your first and last name:\n";
+    cin >> first_name >> last_name;
+    new_line();
+
+    record_name = last_name + ", " + first_name;
+    cout << "Your name in our records is: ";
+    cout << record_name << endl;
+
+    cout << "Our motto is:\n"
+         << motto << endl;
+    cout << "Please suggest a better (one-line) motto: \n";
+    getline(cin, motto);
+    cout << "Our new motto will be:\n";
+    cout << motto << endl;
+
+    return 0;
 }
 
 void new_line()
